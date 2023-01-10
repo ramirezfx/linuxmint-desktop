f52717d84f058a307902b63b145378538713fc14
ramirezfx <68792418+ramirezfx@users.noreply.github.com>
2023-01-10 20:26:08 +0100

    Update custom.sh

--- custom.sh
+++ custom.sh
@@ -3,6 +3,7 @@
 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O /tmp/google-chrome-stable_current_amd64.deb && \
 apt-get install -y /tmp/google-chrome-stable_current_amd64.deb
+rm /tmp/google-chrome-stable_current_amd64.deb
 
 # Install Seafile-Cloud-Sync-Client
 # ---------------------------------
@@ -19,6 +20,4 @@ apt install -y seafile-gui
 # Cleanup
 # -------
 
-rm -Rf /var/cache/apt/archives/
-apt-get remove -y snapd
-rm -Rf /var/lib/snapd
+rm -Rf /var/cache/apt/archives
