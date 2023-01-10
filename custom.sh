4adea23b427d4fc0f79e7c88a6b8cf1b550a3060
ramirezfx <68792418+ramirezfx@users.noreply.github.com>
2023-01-10 20:08:41 +0100

    Update custom.sh

--- custom.sh
+++ custom.sh
@@ -1,4 +1,20 @@
+# Download and install latest Google Chrome-Browser
+# -------------------------------------------------
 
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O /tmp/google-chrome-stable_current_amd64.deb && \
+apt-get install -y /tmp/google-chrome-stable_current_amd64.deb
+
+# Install Seafile-Cloud-Sync-Client
+# ---------------------------------
+
+wget https://linux-clients.seafile.com/seafile.asc -O /usr/share/keyrings/seafile-keyring.asc
+OSRELEASE=$(cat /etc/os-release | grep UBUNTU_CODENAME | cut -d "=" -f2)
+if [ $OSRELEASE == 'jammy'  ]
+then
+ echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/seafile-keyring.asc] https://linux-clients.seafile.com/seafile-deb/jammy/ stable main' > /etc/apt/sources.list.d/seafile.list
+fi
+apt update
+apt install -y seafile-gui
 
 # Cleanup
 # -------
