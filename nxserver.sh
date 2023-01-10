6e790bfb180bd4d8c43a4b1d8b526d9468ba2d85
ramirezfx <68792418+ramirezfx@users.noreply.github.com>
2023-01-10 13:39:49 +0100

    Update nxserver.sh

--- nxserver.sh
+++ nxserver.sh
@@ -6,8 +6,6 @@ groupadd -r $USER -g 433 \
 && echo "setxkbmap "$KBD_LANG > /home/$USER/.bashrc \
 && chown -R $USER:$USER /home/$USER \
 && echo $USER':'$PASSWORD | chpasswd
-echo $LANG UTF8 > /etc/locale.gen && \
-     update-locale LANG=$LANG LANGUAGE
 ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
-service ssh start && /etc/NX/nxserver --startup
+/etc/NX/nxserver --startup
 tail -f /usr/NX/var/log/nxserver.log
