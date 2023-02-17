b3ff73c4cbfcb1e57c2227951961ba56c43a8668
ramirezfx <68792418+ramirezfx@users.noreply.github.com>
2023-02-17 22:56:58 +0100

    Update nxserver.sh

--- nxserver.sh
+++ nxserver.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-# Create User and Group:
+# Install Language-Packs
 apt-get install -y language-pack-$LANGSHORTNAME
 apt-get install -y language-pack-gnome-$LANGSHORTNAME
-rm -Rf /etc/localtime
+
+# Create User and Group:
+
 groupadd -r $USER -g 433 \
 && useradd -u 431 -r -g $USER -d /home/$USER -s /bin/bash -c "$USER" $USER \
 && adduser $USER sudo \
@@ -13,6 +15,7 @@ groupadd -r $USER -g 433 \
 userdel -r mint
 
 # Set Timezone
+rm -Rf /etc/localtime
 ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 
 # Set System-Language:
